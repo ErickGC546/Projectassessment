16fbe4c20bdbe9c3bcb2569caffe0bb4f4c9e756
ErickGC546 <erick.galindo.c@tecsup.edu.pe>
2025-08-25 22:47:18 -0500

    Commit2

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import Header from './components/Header';
 import Banner from './components/Banner';
 import Banner2 from './components/Banner2';
 import ProductGrid from './components/ProductGrid';
-import Footer from './components/footer';
+import Footer from './components/Footer';
 import Categoria from "./components/Categoria";
 import './App.css';
 
